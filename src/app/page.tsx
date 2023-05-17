998b9992c6a3dd7b214fd17a5acd562d09c217ac
Rahul Mondal <rahulmondar@gmail.com>
2023-05-17 07:29:55 +0530

    feat: added a mutation for testing purpose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,15 @@ import { trpc } from "@/utils/trpc";
 import Image from "next/image";
 
 export default function Home() {
-    console.time("radha");
-    const { data, isLoading } = trpc.radha.shyam.useQuery();
-    console.timeEnd("radha");
+    const { mutate } = trpc.radha.kanhaiya.useMutation({
+        onSuccess: (data) => console.log("data :", data),
+    });
 
-    console.log({ data, isLoading });
+    const handleClick = () => {
+        console.time("radha");
+        mutate();
+        console.timeEnd("radha");
+    };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -20,7 +24,7 @@ export default function Home() {
                 </p>
             </div>
 
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+            <div className="relative flex flex-col gap-8 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
                 <Image
                     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
                     src="/next.svg"
@@ -29,6 +33,12 @@ export default function Home() {
                     height={37}
                     priority
                 />
+                <button
+                    onClick={handleClick}
+                    className="px-4 py-2 text-white bg-blue-700 hover:bg-blue-600 rounded transform active:scale-[0.98]"
+                >
+                    Hit Me 🫵🏻
+                </button>
             </div>
 
             <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
--- src/server/routers/radha.ts
+++ src/server/routers/radha.ts
@@ -2,4 +2,8 @@ import { procedure, router } from "@/server/initTrpc";
 
 export const radha = router({
     shyam: procedure.query(() => "RadheShyam❤️RadheShyam"),
+    kanhaiya: procedure.mutation(() => ({
+        success: true,
+        message: "RadheShyam❤️RadheShyam",
+    })),
 });
